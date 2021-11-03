2e3d354842f6852e9f57ee52481e1445ae6a28e7
Maxim Levandovskiy <Levan.Maxim@gmail.com>
2021-11-03 17:46:22 +0200

    Задание 2
    Добавил решение

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const TotalingredientsListEl = [];
+
+ingredients.forEach((item) => {
+  const ingredientsItemEl = document.createElement("li");
+  ingredientsItemEl.classList.add("item");
+  ingredientsItemEl.textContent = item;
+  TotalingredientsListEl.push(ingredientsItemEl);
+});
+
+ingredientsListEl.append(...TotalingredientsListEl);
