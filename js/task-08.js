2d5feee1ce9bdec90e899d827e20f644b276510c
Maxim Levandovskiy <Levan.Maxim@gmail.com>
2021-11-05 14:32:42 +0200

    Задание 8
    Трудно, но сделал!

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
+const loginFormEl = document.querySelector("form.login-form");
 
+loginFormEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  // .preventDefault() - не дает форме перезагружаться
+  event.preventDefault();
+  //   console.log("Это сабмит формы");
+
+  // ссылка на все элемнты
+  const formElements = event.currentTarget.elements;
+
+  // ссылки на значения элементов
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+  //   console.log(mail, password);
+
+  // проверяем, что все поля заполнены
+  if (mail === "" || password === "") {
+    return alert(`Заполните все поля!`);
+  }
+  const onFormResult = {};
+  const formData = new FormData(event.currentTarget);
+  console.log(formData);
+  formData.forEach((value, name) => {
+    onFormResult[name] = value;
+  });
+  console.log(onFormResult);
+  loginFormEl.reset();
+}
