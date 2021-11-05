eef44555b3a375b4ddf4fe1b189a519beba79b27
Maxim Levandovskiy <Levan.Maxim@gmail.com>
2021-11-05 16:39:47 +0200

    Update task-09.js
    готово 9 задание

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// находим кнопку, спан и body
+const btnChangeColorEl = document.querySelector("button.change-color");
+console.log(btnChangeColorEl);
+const spanColorEl = document.querySelector("span.color");
+console.log(spanColorEl);
+const bodyEl = document.querySelector("body");
+console.log(bodyEl);
+
+// отслеживаем событие и по клику вызываем функцию switchColor
+btnChangeColorEl.addEventListener("click", switchColor);
+
+function switchColor() {
+  //вызываем функцию getRandomHexColor() и записываем значение в randomColor
+  const randomColor = getRandomHexColor();
+  // присваиваем значение цвета body
+  bodyEl.style.backgroundColor = randomColor;
+  // записываем значение цвета в span
+  spanColorEl.textContent = randomColor;
+}
