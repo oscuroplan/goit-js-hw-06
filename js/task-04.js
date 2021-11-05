e2db683edbac37b5f239b84d3968916e241e16bd
Maxim Levandovskiy <Levan.Maxim@gmail.com>
2021-11-05 15:42:20 +0200

    Update task-04.js
    в

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,16 @@
 const decrementBtn = document.querySelector("[data-action=decrement]");
 const incrementBtn = document.querySelector("[data-action=increment]");
 const valueEl = document.querySelector("#value");
-
+// создаем хранилище актуального значения
 let counterValue = 0;
 
+// по клику отнимаем значение от актуального
 decrementBtn.addEventListener("click", () => {
   counterValue -= 1;
   valueEl.textContent = counterValue;
 });
 
+// по клику добавляем значение к актуальному
 incrementBtn.addEventListener("click", () => {
   counterValue += 1;
   valueEl.textContent = counterValue;
